03cbafa86239c1c6e344d6bd9e4817173cdd507a
safaroww <asifseferov00@gmail.com>
2022-06-21 13:40:39 +0400

    hmw-oop

---
+++ hmwoop.py
@@ -0,0 +1,57 @@
+#1
+# class Termometr:
+#     def __init__(self, celcius):
+#         self.celcius = celcius
+
+#     def get_kelvin(self):
+#         kelvin = self.celcius + 273
+#         return kelvin
+
+#     def get_fah(self):
+#         fah = (self.celcius * 1.8) + 32
+#         return fah 
+
+#     def get_celc(self):
+#         return self.celcius
+
+#     def fah_to_celc(self):
+#         cel = (self.celcius - 32) / 1.8
+#         return cel
+
+#     def kel_to_celc(self):
+#         cel = self.celcius - 273
+#         return cel
+
+#     def add_fah(self, added):
+#         fah = self.get_fah()
+#         return fah.__add__(added)
+        
+#     def sub_fah(self, subbed):
+#         fah = self.get_fah()
+#         return fah.__sub__(subbed)
+
+#     def add_kel(self, added):
+#         kel = self.get_kelvin()
+#         return kel.__add__(added)
+
+#     def sub_kel(self, subbed):
+#         kel = self.get_kelvin()
+#         return kel.__sub__(subbed)
+    
+#     def add_cel(self, added):
+#         cel = self.get_celc()
+#         return cel.__add__(added)
+    
+#     def sub_cel(self, subbed):
+#         cel = self.get_celc()
+#         return cel.__sub__(subbed)
+
+#     def show_temp(self):
+#         return self.celcius
+
+
+
+
+
+
+
